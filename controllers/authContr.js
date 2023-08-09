79c3aafa4716a04ebc7b8788bc2afc409a5cba1d
Loki99911 <nikita_kresik@i.ua>
2023-08-09 09:21:24 +0300

    Update authContr.js

--- controllers/authContr.js
+++ controllers/authContr.js
@@ -14,7 +14,7 @@ const login = async (req, res) => {
     throw HttpError(401, "Login or password is wrong");
   }
 
-  const passwordCompare = bcrypt.compare(password, user.password);
+  const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw HttpError(401, "Login or password is wrong");
   }
